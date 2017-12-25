8860e1624c23a044d428a6c80c60c27f01843999
Rio-Sh <32601804+Rio-Sh@users.noreply.github.com>
2017-12-25 09:38:56 +0900

    Add files via upload

--- chap8.R
+++ chap8.R
@@ -10,18 +10,18 @@ logL(0.30)
 
 MHM <- function(iter){
   param_vec <- c()
-  init_q <- sample(q_area,1)
+  init_q <- sample(q_area,1)# q_areaからランダムに一つ
   param_vec <- append(param_vec,init_q)
   for (n in 1:iter){
-    q_new <- param_vec[n] + sample(coin,1)
+    q_new <- param_vec[n] + sample(coin,1)#n回目のqに確率で+0.01か-0.01
     if (logL(param_vec[n]) < logL(q_new)){
-    param_vec <- append(param_vec,q_new)
+    param_vec <- append(param_vec,q_new)#logL(q)<logL(q_new)ならq_newへ移動
     }
-    else{
-      r <- exp(logL(q_new)-logL(param_vec[n]))
-      HorT <- sample(c(1,0),size=1,prob=c(r,1-r))
-      if (HorT == T){param_vec <- append(param_vec, q_new)}
-      else{param_vec <- append(param_vec, param_vec[n])}
+    else{#logL(q)>logL(q_new)のとき
+      r <- exp(logL(q_new)-logL(param_vec[n]))#尤度比 r = L(q_new)/L(q)
+      HorT <- sample(c(TRUE,FALSE),size=1,prob=c(r,1-r))#確率rでTRUEになる
+      if (HorT == TRUE){param_vec <- append(param_vec, q_new)}#TRUEがでたらq_newへ移動
+      else{param_vec <- append(param_vec, param_vec[n])}#FALSEならqは移動しない
     }
   }
   return(param_vec)
@@ -37,3 +37,4 @@ par(new=TRUE)
 plot(trace_2,type='l',col=2,ylim=c(0.2,0.7),xlab='',ylab='')
 par(new=TRUE)
 plot(trace_3,type='l',col=3,ylim=c(0.2,0.7),xlab='',ylab='')
+
